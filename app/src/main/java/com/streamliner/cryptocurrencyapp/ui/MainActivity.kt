552ebd565972287e8ace2c903f3ec1a9ad179ce1
tiger <thisisstreamliner@gmail.com>
2022-12-12 02:35:52 +0200

    Migrate kotlin version to 1.7.20 Enable Compose Add Theme

--- app/src/main/java/com/streamliner/cryptocurrencyapp/ui/MainActivity.kt
+++ app/src/main/java/com/streamliner/cryptocurrencyapp/ui/MainActivity.kt
@@ -2,10 +2,20 @@ package com.streamliner.cryptocurrencyapp.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.streamliner.cryptocurrencyapp.ui.theme.CryptoCurrencyAppTheme
 
 class MainActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContent {
+            CryptoCurrencyAppTheme {
+                Surface(color = MaterialTheme.colors.background) {}
+            }
+        }
     }
 }
