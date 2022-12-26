afcc786b547460d56136cbb13b1e16cba6b47c99
tiger <thisisstreamliner@gmail.com>
2022-12-26 23:33:05 +0200

    Add Navigation

--- app/src/main/java/com/streamliner/cryptocurrencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/streamliner/cryptocurrencyapp/presentation/MainActivity.kt
@@ -5,8 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.streamliner.cryptocurrencyapp.presentation.coin_detail.CoinDetailScreen
+import com.streamliner.cryptocurrencyapp.presentation.coin_list.CoinListScreen
 import com.streamliner.cryptocurrencyapp.ui.theme.CryptoCurrencyAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +21,24 @@ class MainActivity: ComponentActivity() {
 
         setContent {
             CryptoCurrencyAppTheme {
-                Surface(color = MaterialTheme.colors.background) {}
+                Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
+                }
             }
         }
     }
